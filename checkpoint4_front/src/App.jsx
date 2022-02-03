ecc7e9412eca6b61efe4a94723346f8e09228750
Kev-rossi <kr.bat@outlook.fr>
2022-02-04 00:36:55 +0100

    added navBar component, update playerConnect

--- checkpoint4_front/src/App.jsx
+++ checkpoint4_front/src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/App.css';
 import PlayerConnect from './components/PlayerConnect';
+// import Navbar from './components/NavBar';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
+      {/* <Navbar /> */}
       <PlayerConnect />
     </div>
   )
---
+++ checkpoint4_front/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import '../styles/navBar.css';
+// import { Link } from 'react-router-dom';
+// import { useState, useContext } from 'react';
+
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className='first-title-nav'>
+        <h1>Borrel card game</h1>
+      </div>
+      <div className='select-nav'>
+        <h2>Connect</h2>
+        <h2>Register</h2>
+      </div>
+      </div>
+  );
+};
+
+export default Navbar;
